6ae6d78ea82aba8014f85e2b066401c5a4aecbd4
Hans Yulian <hansyulian@windowslive.com>
2017-11-16 14:03:45 +0800

    hans version of iframe

--- iFrame.js
+++ iFrame.js
@@ -1,52 +1,52 @@
-namespace("leesa.visual").iFrame = (function(leesa, _, leesaChart) {
-  //#region Constant
-  var observable = ko.observable;
-  var url = "https://console.api.ai/api-client/demo/embedded/2d694bc7-1cbc-4f83-bc18-f15991389822";
-  //#endregion
+namespace("leesa.visual").iFrame = (function (leesa, _, leesaChart) {
+	//#region Constant
+	var observable = ko.observable;
+	//#endregion
 
-  var iFrameChart = {
-    fadeIn: function(quadrant, option) {
-      option.callback();
-    },
-    fadeOut: function(quadrant, option) {
-      option.callback();
-    },
-    extend: function(quadrant) {
-      if (quadrant._extended("iFrame"))
-        return;
+	var iFrameChart = {
+		fadeIn: function (quadrant, option) {
+			option.callback();
+		},
+		fadeOut: function (quadrant, option) {
+			option.callback();
+		},
+		extend: function (quadrant) {
+			if (quadrant._extended("iFrame"))
+				return;
 
-      $.extend(true,
-        quadrant, {
-          _chartObjects: observable(),
-          trend: observable(),
-        });
-    },
-    pushDatum: function pushDatum(quadrant, datumOrData) {},
-    render: function render(quadrant, callback) {
-      var content = quadrant.htmlJContent();
-      var visual = quadrant.visual();
-      var data = quadrant.data();
-      content.html("");
+			$.extend(true,
+				quadrant, {
+					_chartObjects: observable(),
+					trend: observable(),
+				});
+		},
+		pushDatum: function pushDatum(quadrant, datumOrData) { },
+		render: function render(quadrant, callback) {
+			var content = quadrant.htmlJContent();
+			var visual = quadrant.visual();
+			var data = quadrant.data();
+			content.html("");
+			var parameters = visual.parameters;
+			var url = parameters.url || "http://www.fusionexgiant.com";
 
-      //pass through template
-      var containers = content;
+			//pass through template
+			var containers = content;
 
-      var jIFrame = $("<iframe />")
-      jIFrame.attr({
-        height: "100%",
-        width: "100%",
-        src: url
-      });
-      jIFrame.appendTo(content);
-      callback(quadrant);
-    },
+			var jIFrame = $("<iframe class='nice-iframe'></iframe>")
+			jIFrame.attr({
+				src: url,
+				frameBorder: 0
+			});
+			jIFrame.appendTo(content);
+			callback(quadrant);
+		},
 
-    configuration: {}
-  };
+		configuration: {}
+	};
 
-  return iFrameChart;
+	return iFrameChart;
 
-  //#region private methods
-  //#endregion
+	//#region private methods
+	//#endregion
 
 })(leesa, _, namespace("leesaChart"));
